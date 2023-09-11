759e9d86d631d9f65f008447babb761464ae270f
Caiyuzhen <caiyuzhen00@gmail.com>
2023-09-12 01:45:47 +0800

    base

---
+++ myapp/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+// 命名空间
+namespace Hey {
+	// 类型
+	class Program123 {
+		// 函数
+		static void Main(string[] args) {
+			int a = 100;
+			int n = 200;
+			int sum = 0;
+			sum = a + n;
+
+			string name = "Hello";
+
+			
+
+			System.Console.WriteLine(sum); //打印输出
+			System.Console.ReadKey(); //接收用户的键盘输入
+		}
+	}
+}
